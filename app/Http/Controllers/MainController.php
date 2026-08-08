43bdb0a87ac7901d7ffa578090711076090f0710
Łukasz Jaroszuk <krortop@gmail.com>
2026-08-08 14:58:55 +0200

    Show elements table

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,10 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Elements;
+
 class MainController extends Controller
 {
     public function list()
     {
-        return view("main", []);
+        $elements = Elements::all();
+        $res = [];
+        foreach ($elements as $e) {
+            $res[$e->rows][$e->columns] = $e->toArray();
+        }
+
+        return view("main", ["res" => $res]);
     }
 }
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -6,6 +6,55 @@
 <a href="/elements" class="btn btn-info">Pierwiastki</a>
 <a href="/fill/Elements" class="btn btn-info">Uzupełnij Pierwiastki</a>
 <div class="container">
+    <table class="elemTable">
+        @for ($i = 1; $i <= 7; $i++)
+            <tr>
+            @for ($j = 1; $j <= 18; $j++)
+                <td @if (!isset($res[$i][$j])) class="noelem" @endif>
+                @if (isset($res[$i][$j]))
+                {{$res[$i][$j]['z']}}<br />
+                {{$res[$i][$j]['short']}}<br />
+                <span class="small">{{$res[$i][$j]['name']}}</span>
+                @endif
+                </td>
+                @endfor
+                </tr>
+                @endfor
+    </table>
+    <br />
+    <h4>Lantanowce</h4>
+    <table class="elemTable">
+
+        <tr>
+            @for ($j = 1; $j <= 14; $j++)
+                <td>
+                @if (isset($res[$i][$j]))
+                {{$res[8][$j]['z']}}<br />
+                {{$res[8][$j]['short']}}<br />
+                <span class="small">{{$res[8][$j]['name']}}</span>
+                @endif
+                </td>
+                @endfor
+        </tr>
+
+    </table>
+    <br />
+    <h4>Aktynowce</h4>
+    <table class="elemTable">
+
+        <tr>
+            @for ($j = 1; $j <= 14; $j++)
+                <td>
+                @if (isset($res[$i][$j]))
+                {{$res[9][$j]['z']}}<br />
+                {{$res[9][$j]['short']}}<br />
+                <span class="small">{{$res[9][$j]['name']}}</span>
+                @endif
+                </td>
+                @endfor
+        </tr>
+
+    </table>
 
 </div>
 
